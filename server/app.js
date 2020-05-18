950631a7ed1dd549e978eff9be206732d473ed75
Kirill Myagkov <kirillmyagkov17@gmail.com>
2020-05-18 19:53:20 +0300

    Config sockets

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 io.on('connection', socket => {
-  console.log(socket, 'connected');
+  
 });
 
 module.exports = {
