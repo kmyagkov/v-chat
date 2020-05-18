ad37450b7e605395eae465a275c8dc191c7a2e46
Kirill Myagkov <kirillmyagkov17@gmail.com>
2020-05-18 17:42:55 +0300

    Config server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
+const consola = require('consola');
+const { Nuxt, Builder } = require('nuxt');
+const { app, server } = require('./app');
 
-const config = require('../nuxt.config.js')
-config.dev = process.env.NODE_ENV !== 'production'
+const config = require('../nuxt.config.js');
+
+config.dev = process.env.NODE_ENV !== 'production';
 
 async function start () {
-  const nuxt = new Nuxt(config)
+  const nuxt = new Nuxt(config);
 
-  const { host, port } = nuxt.options.server
+  const { host, port } = nuxt.options.server;
 
   await nuxt.ready()
   if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+    const builder = new Builder(nuxt);
+    await builder.build();
   }
 
-  app.use(nuxt.render)
+  app.use(nuxt.render);
 
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
+  server.listen(port, () => {
+    consola.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true
+    })
+  });
 }
 
-start()
+start();
